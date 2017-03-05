d59da83afdd192f579fa0b295c617b621f9eced4
AlekseyMA <muschenko.aa@gmail.com>
2017-03-05 11:50:59 +0500

    kill

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,24 +12,14 @@ namespace Snake
         static void Main(string[] args)
         {
             Console.SetBufferSize(80, 25);
-            
-            
-            //Отрисовка линий
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Drow();
-            downLine.Drow();            
-            leftLine.Drow();
-            rightLine.Drow();
 
-
-
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+           
             //Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
+            snake.Draw();
 
             FoodCreator foodCreator = new FoodCreator(80, 25, '#');
             Point food = foodCreator.CreateFood();
@@ -37,6 +27,12 @@ namespace Snake
 
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                    {
+                    break;
+                    }
+
+
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
